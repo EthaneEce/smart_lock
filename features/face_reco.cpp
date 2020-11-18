f5a9d4c441e7fd3763a1decec06725acc7607dff
MrKiutek <kiutek@outlook.fr>
2020-11-18 14:02:58 +0100

    Add missing includes

--- features/face_reco.cpp
+++ features/face_reco.cpp
@@ -1,6 +1,27 @@
 
-#include "face_reco.h"
+#include "opencv2/opencv.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/objdetect.hpp"
+#include "opencv2/face.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
 
+#include <raspicam/raspicam_cv.h>
+
+#include <iostream>
+#include <ctime>
+#include <fstream>
+#include <map>
+#include <csignal>
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 using namespace cv;
 using namespace cv::face;
 using namespace std;
