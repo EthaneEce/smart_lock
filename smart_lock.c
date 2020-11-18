edbe33180bee569610db057aebbc9021a9d3ea91
EthaneEce <46593602+EthaneEce@users.noreply.github.com>
2020-11-18 09:50:26 +0100

    Update smart_lock.c

--- smart_lock.c
+++ smart_lock.c
@@ -6,5 +6,12 @@
 
 int main(){
     printf("Hello World!\n");
+    static pid_t pid = 0;
+
+    if ((pid = fork()) == 0) {
+        system("./read_rfid_uid.py");
+        printf("%d", pid);
+    }
+    
     start_reco();
 }
