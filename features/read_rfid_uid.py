13fc6b7490e62af8f318a76202f30ec0bf3e0d33
EthaneEce <46593602+EthaneEce@users.noreply.github.com>
2020-11-18 17:45:22 +0100

    Update read_rfid_uid.py

--- features/read_rfid_uid.py
+++ features/read_rfid_uid.py
@@ -7,29 +7,8 @@ import time
 
 GPIO.setmode(GPIO.BOARD) #Définit le mode de numérotation (Board)
 GPIO.setwarnings(False) #On désactive les messages d'alerte
-LED_RED = 3 #Définit le numéro du port GPIO qui alimente la led rouge
-LED_GREEN = 5 #Définit le numéro du port GPIO qui alimente la led verte
+
 RFID_UID = [ 243, 217, 126, 36, 112]
-
-def turn_led_on (led) :
-    GPIO.setup(led, GPIO.OUT) #Active le contrôle du GPIO
-    GPIO.output(led, GPIO.HIGH) #Allume la led
-
-#Définit la fonction permettant d'éteindre une led
-def turn_led_off (led) :
-    GPIO.setup(led, GPIO.OUT) #Active le contrôle du GPIO
-    GPIO.output(led, GPIO.LOW) #Eteind la led
-
-#Définit la fonction permettant d'allumer la rouge et éteindre la verte
-def turn_red_on () :
-    turn_led_off(LED_GREEN) #Eteind la led verte
-    turn_led_on(LED_RED) #Allume la led rouge
-
-#Définit la fonction permettant d'allumer la verte et éteindre la rouge
-def turn_green_on () :
-    turn_led_off(LED_RED) #Eteind la led rouge
-    turn_led_on(LED_GREEN) #Allume la led verte
-
 
 rc522 = RFID() #On instancie la lib
 
@@ -41,7 +20,6 @@ while True :
     file.truncate()
     file.write("0")
     file.close()
-    turn_red_on()
     rc522.wait_for_tag() #On attnd qu'une puce RFID passe à portée
     print('tag readed')
     (error, tag_type) = rc522.request() #Quand une puce a été lue, on récupère ses infos
@@ -59,11 +37,13 @@ while True :
             file.truncate()
             file.write("1")
             file.close()
-            turn_green_on()
             time.sleep(2)
         else :
             print('Badge {} interdit !'.format(uid))
-            turn_red_on()
+            file = open("rfid_is_accepted.txt", "w")
+            file.truncate()
+            file.write("0")
+            file.close()
     
     time.sleep(0.2) #On attend 1 seconde pour ne pas lire le tag des centaines de fois en quelques milli-secondes
     
